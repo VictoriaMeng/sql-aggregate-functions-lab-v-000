597528a15bb0c4d4afd885a55714f0264ecc824b
Victoria Meng <vym205@protonmail.com>
2019-04-28 23:01:45 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,6 +1,12 @@
 CREATE TABLE students
-    (id INTEGER PRIMARY KEY,
-    name TEXT,
-    grade INTEGER,
-    gpa FLOAT,
-    tardies INTEGER);
+  (id INTEGER PRIMARY KEY,
+  name TEXT,
+  grade INTEGER,
+  gpa FLOAT,
+  tardies INTEGER);
+-- CREATE TABLE students
+--     (id INTEGER PRIMARY KEY,
+--     name TEXT,
+--     grade INTEGER,
+--     gpa FLOAT,
+--     tardies INTEGER);
